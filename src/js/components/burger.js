dc2e1682be40466e609e2e34f18465d2afed7005
Your Name <you@example.com>
2023-07-20 16:24:25 +0300

    adaptive

---
+++ src/js/components/burger.js
@@ -0,0 +1,10 @@
+const menuBurger = document.querySelector('.menu-burger');
+const menu = document.querySelector('.menu');
+
+menuBurger.addEventListener('click', () => {
+  // Переключаем класс menu--active для отображения/скрытия меню
+  menu.classList.toggle('menu--active');
+
+  // Переключаем класс menu-burger--active для смены иконки бургера на крестик и обратно
+  menuBurger.classList.toggle('menu-burger--active');
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
-import './components/transactions';
 import './components/enrollments';
+import './components/transactions';
+import './components/burger';
 import './components/modal-rent';
 import './_vendor';
 import vars from './_vars';
