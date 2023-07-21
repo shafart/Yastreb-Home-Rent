4f3e94eba8e2138b83dd5e42ed3ef98176f8d703
Your Name <you@example.com>
2023-07-21 16:26:26 +0300

    adaptive v2

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -1,10 +1,10 @@
-const menuBurger = document.querySelector('.menu-burger');
-const menu = document.querySelector('.menu');
+// const menuBurger = document.querySelector('.menu-burger');
+// const menu = document.querySelector('.menu');
 
-menuBurger.addEventListener('click', () => {
-  // Переключаем класс menu--active для отображения/скрытия меню
-  menu.classList.toggle('menu--active');
+// menuBurger.addEventListener('click', () => {
+//   // Переключаем класс menu--active для отображения/скрытия меню
+//   menu.classList.toggle('menu--active');
 
-  // Переключаем класс menu-burger--active для смены иконки бургера на крестик и обратно
-  menuBurger.classList.toggle('menu-burger--active');
-});
+//   // Переключаем класс menu-burger--active для смены иконки бургера на крестик и обратно
+//   menuBurger.classList.toggle('menu-burger--active');
+// });
--- src/js/components/modal-rent.js
+++ src/js/components/modal-rent.js
@@ -26,3 +26,14 @@ document.addEventListener('click', (event) => {
     });
   }
 });
+
+const menuBurger = document.querySelector('.menu-burger');
+const menu = document.querySelector('.menu');
+
+menuBurger.addEventListener('click', () => {
+  // Переключаем класс menu--active для отображения/скрытия меню
+  menu.classList.toggle('menu--active');
+
+  // Переключаем класс menu-burger--active для смены иконки бургера на крестик и обратно
+  menuBurger.classList.toggle('menu-burger--active');
+});
