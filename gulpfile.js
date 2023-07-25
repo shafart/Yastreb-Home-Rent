9d5d4d5da7b93661ee7565e0446e350afb58ae53
Your Name <you@example.com>
2023-07-25 15:20:34 +0300

    gh path img test

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ const rootFolder = path.basename(path.resolve());
 
 // paths
 const srcFolder = './src';
-const buildFolder = './app';
+const buildFolder = './docs';
 const paths = {
   srcSvg: `${srcFolder}/img/svg/**.svg`,
   srcImgFolder: `${srcFolder}/img`,
@@ -277,7 +277,7 @@ const cache = () => {
 };
 
 const rewrite = () => {
-  const manifest = readFileSync('app/rev.json');
+  const manifest = readFileSync('docs/rev.json');
   src(`${paths.buildCssFolder}/*.css`)
     .pipe(revRewrite({
       manifest
